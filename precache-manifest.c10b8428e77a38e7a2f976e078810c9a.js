ef399ab14fa32dae73066dcc9454f3f3abc2a71d
Elissa <elissathomas@Elissas-MacBook-Pro.local>
2018-10-25 09:43:55 -0700

    Updates

---
+++ precache-manifest.c10b8428e77a38e7a2f976e078810c9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a07be2c9bc3792a5807f",
+    "url": "/Game-of-Life/static/js/runtime~main.a07be2c9.js"
+  },
+  {
+    "revision": "9300c56158bef3ca1267",
+    "url": "/Game-of-Life/static/js/main.9300c561.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/Game-of-Life/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "9300c56158bef3ca1267",
+    "url": "/Game-of-Life/static/css/main.25ded710.chunk.css"
+  },
+  {
+    "revision": "7c879977a6dd91ddd6f0668137cf5c14",
+    "url": "/Game-of-Life/index.html"
+  }
+];
